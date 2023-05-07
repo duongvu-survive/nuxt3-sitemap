57695fc3c106831fecb858fd33b8bfe7af923b56
duongvu <duongvu.survive@gmail.com>
2023-05-08 06:40:33 +0700

    fix cache with locale

--- dist/runtime/sitemap.mjs
+++ dist/runtime/sitemap.mjs
@@ -6,19 +6,32 @@ import { excludeRoutes } from '~sitemap/runtime/routes.mjs'
 import { createRoutesCache } from '~sitemap/runtime/cache.mjs'
 import { useRuntimeConfig } from '#internal/nitro'
 
-export const globalCache = { cache: {  }, staticRoutes: null }
+export const globalCache = { cache: {}, staticRoutes: null }
+
+const getLocaleFromHost = (host) => {
+  var strs = host.split('.')
+  if (strs.length == 2) {
+    return 'en'
+  }
+  return strs[0]
+}
 
 export default eventHandler(async (event) => {
   const runtimeConfig = useRuntimeConfig()
   const res = event.res
   const req = event.req
-  process.env.DOMAIN_SITEMAP = req.headers.host
+  process.env.LOCALE = getLocaleFromHost(req.headers.host)
+  const KEY_CACHE = process.env.LOCALE + event.req.url
+
+
   const require = createRequire(import.meta.url)
   if (!require) {
     console.log('cant use require in middleware')
   }
   // eslint-disable-next-line no-new-func,no-eval
+
   const options = eval('(' + runtimeConfig.sitemap.options + ')')[event.req.url]
+
   const staticRoutes = runtimeConfig.sitemap.staticRoutes
 
   // Init cache
@@ -26,13 +39,14 @@ export default eventHandler(async (event) => {
     globalCache.staticRoutes = () => excludeRoutes(options.exclude, staticRoutes)
   }
 
-  if(!globalCache.cache[event.req.url]) {
-    globalCache.cache[event.req.url] = createRoutesCache(globalCache, options)
+  if (!globalCache.cache[KEY_CACHE]) {
+    globalCache.cache[KEY_CACHE] = createRoutesCache(globalCache, options)
   }
 
   try {
     // Init sitemap
-    const routes = await globalCache.cache[event.req.url].get('routes')
+
+    const routes = await globalCache.cache[KEY_CACHE].get('routes')
     const xml = createSitemap(options, routes, options.base, req).toXML()
     // Check cache headers
     if (validHttpCache(xml, options.etag, req, res)) {
